bd37c0f7e13c2d2503f70198d660f8f157bc9378
mlanzamolinapb <mlanzamolina@gmail.com>
2022-09-11 22:20:06 -0600

    cxreating dev branch

---
+++ ReportSystem.BranchOffices/Controllers/BranchOfficeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportSystem.BranchOffices.Controllers
+{
+    public class BranchOfficeController : Controller
+    {
+        // GET: BranchOfficeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BranchOfficeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BranchOfficeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BranchOfficeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BranchOfficeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BranchOfficeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BranchOfficeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BranchOfficeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
