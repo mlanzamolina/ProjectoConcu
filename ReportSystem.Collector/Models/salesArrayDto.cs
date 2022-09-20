9f8cef832c7534c29f576f65538456c4a99a830a
MarioVelasquezN <rolando.velasquez18@hotmail.com>
2022-09-19 20:40:15 -0600

    collector sends data from file.json

---
+++ ReportSystem.Collector/Models/salesArrayDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportSystem.Collector.Models
+{
+    internal class salesArrayDto
+    {
+        public salesDto [] list  { get; set; }
+    }
+}
